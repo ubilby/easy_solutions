19f8947f61fac3293751c702add382032eda6b34
ubilby <ubilby@gmail.com>
2023-12-05 02:00:29 +0300

    add item model

--- payments/items/models.py
+++ payments/items/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models):
+    name = models.CharField(verbose_name='Имя', max_length=80, blank=False)
+    description = models.CharField(verbose_name='Описание', max_length=500)
+    price = models.DecimalField(
+        verbose_name='Цена', max_digits=10, decimal_places=2
+    )
